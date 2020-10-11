a4556ef4f0fd4f951d6910c4d51656e90bb0ede4
Shaun Wassell <shaun@trove.ai>
2020-10-11 17:32:38 -0400

    Completed app

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { DashboardPage } from './dashboard';
 
-const routes = [ ];
+const routes = [{
+    path: '/',
+    exact: true,
+    Component: DashboardPage,
+}];
 
 export const Routes = () => (
     <Router>
